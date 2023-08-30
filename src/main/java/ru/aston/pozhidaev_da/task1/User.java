51918dda81a154fa3183fe0276d553280750be74
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2023-08-30 17:50:10 +0300

    final commit

--- src/main/java/ru/aston/pozhidaev_da/task1/User.java
+++ src/main/java/ru/aston/pozhidaev_da/task1/User.java
@@ -1,8 +1,8 @@
 package ru.aston.pozhidaev_da.task1;
 
 public class User {
-    private int age;
     private String name;
+    private int age;
     private String surname;
 
     public User(int age, String name, String surname) {
