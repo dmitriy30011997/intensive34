60127bbd286eb7d4bd9d26db23bc941a7764d2a0
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2023-09-04 10:33:37 +0300

    added treeset, arraylist, hashmap

--- src/test/java/ru/aston/pozhidaev_da/task1/OrderManagerTest.java
+++ src/test/java/ru/aston/pozhidaev_da/task1/OrderManagerTest.java
@@ -1,10 +1,14 @@
 package ru.aston.pozhidaev_da.task1;
 
-import java.util.List;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.Assert;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.Map;
+import static org.junit.Assert.*;
 
 public class OrderManagerTest {
     private OrderManager orderManager;
@@ -13,6 +17,7 @@ public class OrderManagerTest {
     public void setUp() {
         orderManager = new OrderManager();
     }
+
     @Test
     public void testAddOrderWithInvalidPrice() {
         try {
@@ -39,6 +44,7 @@ public class OrderManagerTest {
             Assert.assertEquals("Invalid discount: Discounted price cannot be zero or negative.", e.getErrorMessage());
         }
     }
+
     @Test
     public void testAddOrder() {
         AbstractProduct product = new NonPrescriptionProduct("Product 1", 10.0, new User(20, "John", "Doe"));
@@ -68,5 +74,102 @@ public class OrderManagerTest {
         expected.add(product1);
         expected.add(product3);
         Assert.assertEquals(expected, orderManager.getOrders());
+    }
+
+    @Test
+    public void testArrayListConstructor() {
+        // Тестирование конструкторов ArrayList
+        List<String> list1 = new ArrayList<>(); // Конструктор по умолчанию
+        List<String> list2 = new ArrayList<>(10); // Конструктор с начальной емкостью
+
+        assertNotNull(list1);
+        assertNotNull(list2);
+    }
+
+    @Test
+    public void testHashMapConstructor() {
+        // Тестирование конструкторов HashMap
+        Map<String, Integer> map1 = new HashMap<>(); // Конструктор по умолчанию
+        Map<String, Integer> map2 = new HashMap<>(10); // Конструктор с начальной емкостью
+
+        assertNotNull(map1);
+        assertNotNull(map2);
+    }
+
+    @Test
+    public void testTreeSetConstructor() {
+        // Тестирование конструктора TreeSet
+        TreeSet<Integer> set = new TreeSet<>();
+
+        assertNotNull(set);
+    }
+
+    @Test
+    public void testArrayListMethods() {
+        // Тестирование основных методов ArrayList
+        List<String> list = new ArrayList<>();
+
+        // Добавление элементов
+        list.add("Item 1");
+        list.add("Item 2");
+        list.add("Item 3");
+
+        // Проверка размера
+        assertEquals(3, list.size());
+
+        // Получение элемента по индексу
+        assertEquals("Item 1", list.get(0));
+
+        // Проверка наличия элемента
+        assertTrue(list.contains("Item 2"));
+
+        // Удаление элемента
+        list.remove("Item 2");
+        assertFalse(list.contains("Item 2"));
+    }
+
+    @Test
+    public void testHashMapMethods() {
+        // Тестирование основных методов HashMap
+        Map<String, Integer> map = new HashMap<>();
+
+        // Добавление элементов
+        map.put("Key 1", 1);
+        map.put("Key 2", 2);
+        map.put("Key 3", 3);
+
+        // Проверка размера
+        assertEquals(3, map.size());
+
+        // Получение значения по ключу
+        assertEquals(Integer.valueOf(2), map.get("Key 2"));
+
+        // Проверка наличия ключа
+        assertTrue(map.containsKey("Key 3"));
+
+        // Удаление элемента по ключу
+        map.remove("Key 2");
+        assertFalse(map.containsKey("Key 2"));
+    }
+
+    @Test
+    public void testTreeSetMethods() {
+        // Тестирование основных методов TreeSet
+        TreeSet<Integer> set = new TreeSet<>();
+
+        // Добавление элементов
+        set.add(3);
+        set.add(1);
+        set.add(2);
+
+        // Проверка размера
+        assertEquals(3, set.size());
+
+        // Проверка наличия элемента
+        assertTrue(set.contains(2));
+
+        // Удаление элемента
+        set.remove(2);
+        ;
     }
 }
